diego-ch/android_device_samsung_janice

boardconfig: add egl and hwcomposer configs

BoardConfig.mk
@@ -22,12 +22,14 @@ TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/janice/overlay/include
 
+# Kernel
 BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
 
+# Filesystem
 BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
@@ -35,6 +37,16 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 4096
 TARGET_USERIMAGES_USE_EXT4 := true
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/janice/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
 
 # Inherit from the proprietary version
 -include vendor/samsung/janice/BoardConfigVendor.mk

onioneq4/android_device_samsung_gavini-old

BoardConfig.mk
@@ -22,12 +22,14 @@ TARGET_RECOVERY_INITRC := device/samsung/gavini/rootdir/recovery.rc
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/gavini/overlay/include
 
+# Kernel
 BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_SOURCE := kernel/samsung/gavini
 TARGET_KERNEL_CONFIG := cyanogenmod_i8530_defconfig
 
+# Filesystem
 BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
@@ -35,6 +37,16 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 4096
 TARGET_USERIMAGES_USE_EXT4 := true
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/gavini/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
 
 # Inherit from the proprietary version
 -include vendor/samsung/gavini/BoardConfigVendor.mk