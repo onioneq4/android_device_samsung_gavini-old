diego-ch/android_device_samsung_janice

re-use galaxys2 charger configs

BoardConfig.mk
@@ -86,5 +86,10 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 
+# Charging mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/devices/virtual/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGER_RES := device/samsung/janice/res/charger
+
 # Inherit from the proprietary version
 -include vendor/samsung/janice/BoardConfigVendor.mk

onioneq4/android_device_samsung_gavini-old

BoardConfig.mk
@@ -86,5 +86,10 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 
+# Charging mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/devices/virtual/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGER_RES := device/samsung/gavini/res/charger
+
 # Inherit from the proprietary version
 -include vendor/samsung/gavini/BoardConfigVendor.mk