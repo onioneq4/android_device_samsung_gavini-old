diego-ch/android_device_samsung_janice

proper releasetools now using galaxys2-common release tools as source, edited filenames and partition numbers.

BoardConfig.mk
@@ -92,8 +92,8 @@ BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGER_RES := device/samsung/janice/res/charger
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/u8500_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/u8500_img_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/janice_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/janice_img_from_target_files
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/janice/shbootimg.mk
 
 TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070

onioneq4/android_device_samsung_gavini-old

BoardConfig.mk
@@ -92,9 +92,9 @@ BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGER_RES := device/samsung/gavini/res/charger
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/gavini/releasetools/u8500_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/gavini/releasetools/u8500_img_from_target_files
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gavini/shbootimg.mk
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/gavini/releasetools/gavini_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/gavini/releasetools/gavini_img_from_target_files
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/janice/shbootimg.mk
 
 TARGET_OTA_ASSERT_DEVICE := gavini,i8530,GT-I8530
 