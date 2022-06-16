diego-ch/android_device_samsung_janice

update fstab to version 2 (unified fstab)
updating fstab we guarantee to be updated with the api, recovery uses
version 2 from upstream too

BoardConfig.mk
@@ -14,4 +14,4 @@ BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrat
 
 # Recovery
 TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
-RECOVERY_FSTAB_VERSION := 1
+TARGET_RECOVERY_FSTAB := device/samsung/janice/rootdir/fstab.samsungjanice

onioneq4/android_device_samsung_gavini-old

BoardConfig.mk
@@ -13,6 +13,5 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gavini/bluetooth
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/gavini/vibrator/vibrator.c
 
 # Recovery
-<<<<<<< HEAD
 TARGET_RECOVERY_INITRC := device/samsung/gavini/rootdir/recovery.rc
-RECOVERY_FSTAB_VERSION := 1
+TARGET_RECOVERY_FSTAB := device/samsung/gavini/rootdir/fstab.samsunggavini
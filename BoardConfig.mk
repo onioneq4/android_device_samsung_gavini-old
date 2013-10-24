efce81c3f193845a0aebde7addeeecaa545935fb
Franco Rapetti <frapeti@gmail.com>
2013-10-24 19:45:32 -0300

    update fstab to version 2 (unified fstab)
    updating fstab we guarantee to be updated with the api, recovery uses
    version 2 from upstream too

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,5 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gavini/bluetooth
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/gavini/vibrator/vibrator.c
 
 # Recovery
-<<<<<<< HEAD
 TARGET_RECOVERY_INITRC := device/samsung/gavini/rootdir/recovery.rc
-RECOVERY_FSTAB_VERSION := 1
+TARGET_RECOVERY_FSTAB := device/samsung/gavini/rootdir/fstab.samsunggavini
