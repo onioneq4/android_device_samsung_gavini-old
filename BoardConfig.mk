diego-ch/android_device_samsung_janice

TARGET_RECOVERY_INITRC added

BoardConfig.mk
@@ -11,3 +11,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/janice/bluetooth
 
 # Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrator.c
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc

onioneq4/android_device_samsung_gavini-old

BoardConfig.mk
@@ -11,3 +11,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gavini/bluetooth
 
 # Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/gavini/vibrator/vibrator.c
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/samsung/gavini/rootdir/recovery.rc